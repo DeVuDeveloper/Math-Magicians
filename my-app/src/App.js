8e0fd13867783a8fd5a7e872f494d0d20427459f
VuDej <drvu47@gmail.com>
2022-02-09 11:53:50 +0100

    made directory logic with operate and calculate gfiles and set events in Calculator class

--- my-app/src/App.js
+++
@@ -1,1 +0,0 @@
-import Calculator from './components/Calculator';
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import Calculator from './components/Calculator';
+
+class App extends react.Component {
+  render() {
+    return <Calculator />;
+  }
+}
+
+export default App;
